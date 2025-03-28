89b4829381809b9a8fb8298aac250df016c388c9
AndreBerti <andreluizberti@gmail.com>
2025-03-28 22:00:33 +0000

    Adiciona arquivos do projeto~

---
+++ criadorAleatorioDados/Peca.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criadorAleatorioDados
+{
+    public class Peca
+    {
+        public string codigo { get; }
+        public DateTime data { get; }
+        public int duracao { get; }
+        public EnumResultadoTeste resultadoTeste { get; }
+        public Contabilidade financa { get; }
+
+        public Peca()
+        {
+            Random random = new Random();
+            codigo = GeraCodigo();
+            data = GeraData();
+            duracao = random.Next(10, 51);
+            resultadoTeste = GerarTeste();
+            financa = new Contabilidade(codigo, duracao, resultadoTeste);
+        }
+
+        public EnumResultadoTeste GerarTeste()
+        {
+            Random random = new Random();
+
+            switch (random.Next(1, 7))
+            {
+                case 1:
+                    return EnumResultadoTeste.OK;
+                case 2:
+                    return EnumResultadoTeste.FalhaInspecaoEstanqueidade;
+                case 3:
+                    return EnumResultadoTeste.FalhaInspecaoResistencia;
+                case 4:
+                    return EnumResultadoTeste.FalhaInspecaoVisual;
+                case 5:
+                    return EnumResultadoTeste.FalhaInspecaoDimensoes;
+                case 6:
+                    return EnumResultadoTeste.Desconhecido;
+                default:
+                    return EnumResultadoTeste.OK; 
+            }
+        }
+
+
+
+        public DateTime GeraData()
+        {
+            Random random = new Random();
+            //Cria Data Aleatória
+            int ano = random.Next(1950, 2025);
+            int mes = random.Next(1, 13);
+            int dia = random.Next(1, DateTime.DaysInMonth(ano, mes) + 1);
+            //Cria Hora Aleatória
+            int hora = random.Next(0, 24);
+            int minuto = random.Next(0, 60);
+            int segundo = random.Next(0, 60);
+            return new DateTime(ano, mes, dia, hora, minuto,segundo);
+        }
+
+        public string GeraCodigo()
+        {
+            Random random = new Random();
+            const string letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int tipo = random.Next(1,5);
+            string codigo="";
+            for (int i = 0; i < 6; i++)
+            {
+                codigo += letras[random.Next(0, letras.Length)];
+            }
+            //selecionando o tipo de peça
+            switch (tipo)
+            {
+                case 1:
+                    codigo = "AA"+codigo;
+                    break;
+                case 2:
+                    codigo = "AB"+codigo;
+                    break;
+                case 3:
+                    codigo = "BA" + codigo;
+                    break;
+                case 4:
+                    codigo = "BB" + codigo;
+                    break;
+                default:
+                    codigo = "ERRO";
+                    break;
+            }
+            return codigo;
+        }
+
+       
+    }
+
+    
+}
---
+++ criadorAleatorioDados/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Globalization;
+
+namespace criadorAleatorioDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configurações
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = Path.Combine(desktopPath, "pecas_financas.csv");
+            int quantidadePecas = 1000;
+
+            try
+            {
+                // Configuração de cultura para garantir ponto decimal
+                CultureInfo culture = CultureInfo.InvariantCulture;
+
+                // Cabeçalho do CSV
+                var csvContent = new StringBuilder();
+                csvContent.AppendLine("Código;Data;Duração(min);ResultadoTeste;CustoProdução;Prejuízo;Lucro");
+
+                Console.WriteLine("Iniciando geração de dados...");
+
+                // Gerar as peças
+                for (int i = 0; i < quantidadePecas; i++)
+                {
+                    var peca = new Peca();
+
+                    // Formatar a linha do CSV
+                    var linha = string.Format(culture,
+                        "{0};{1:yyyy-MM-dd HH:mm:ss};{2};{3};{4:F2};{5:F2};{6:F2}",
+                        peca.codigo,
+                        peca.data,
+                        peca.duracao,
+                        peca.resultadoTeste,
+                        peca.financa.custoProducao,
+                        peca.financa.prejuizo,
+                        peca.financa.lucro);
+
+                    csvContent.AppendLine(linha);
+
+                    // Mostrar progresso a cada 100 peças
+                    if ((i + 1) % 100 == 0)
+                        Console.WriteLine($"Geradas {i + 1} de {quantidadePecas} peças");
+                }
+
+                // Escrever no arquivo
+                File.WriteAllText(path, csvContent.ToString(), Encoding.UTF8);
+
+                Console.WriteLine($"\nArquivo gerado com sucesso em: {path}");
+                Console.WriteLine($"Total de peças: {quantidadePecas}");
+                Console.WriteLine("Pressione qualquer tecla para sair...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
